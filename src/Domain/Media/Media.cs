764596c56e9b226f2331ffea9bcdb2ea3fc969da
maxdnes <dnestreanschiimaxim780@gmail.com>
2025-11-19 10:33:17 +0100

    Added initial interfaces

---
+++ src/Domain/Media/Media.cs
@@ -0,0 +1,10 @@
+﻿namespace oop.Domain.Media;
+
+public abstract class Media
+{
+    string Title;
+    string Language;
+    bool IsBorrowed;
+    // TODO: Replace int with borrower
+    Dictionary<int,int> Ratings; 
+}
