c69f2e959388c87594fcd22817efc58e6295a6da
maxdnes <dnestreanschiimaxim780@gmail.com>
2025-11-19 10:55:44 +0100

    Added movie type

--- src/Domain/Media/Media.cs
+++ src/Domain/Media/Media.cs
@@ -2,9 +2,22 @@
 
 public abstract class Media
 {
-    string Title;
-    string Language;
-    bool IsBorrowed;
-    // TODO: Replace int with borrower
-    Dictionary<int,int> Ratings; 
+    public string Title { get; set; }
+    public bool IsBorrowed { get; set; }
+    
+    // TODO: change from string to Rating after implemented 
+    public Dictionary<string, int> Ratings { get; set; } 
+
+    protected Media(string title)
+    {
+        Title = title;
+    }
+
+    public void Borrow() => IsBorrowed = true;
+    public void Return() => IsBorrowed = false;
+
+    public double GetAverageRating()
+    {
+        return Ratings.Values.Average();
+    }
 }
---
+++ src/Domain/Media/Movie.cs
@@ -0,0 +1,29 @@
+﻿using oop.Domain.Interfaces;
+
+namespace oop.Domain.Media;
+
+public class Movie : Media, IWatchable
+{
+    private string Director;
+    private string Genre;
+    private int ReleaseYear;
+    private int Duration;
+    public string Language;
+    
+    public Movie(string title, string language, string director, string genre,
+        int releaseYear, int duration)
+        : base(title)   // calls the Media constructor
+    {
+        Director = director;
+        Genre = genre;
+        ReleaseYear = releaseYear;
+        Duration = duration;
+        Language = language;
+    }
+
+
+    public void Watch()
+    {
+        throw new NotImplementedException();
+    }
+}
